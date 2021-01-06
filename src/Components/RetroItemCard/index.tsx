b91f751c0361747f18e30a0d55a6d238c15db39d
Ryan Foley <rtfoley@gmail.com>
2021-01-06 00:25:21 -0500

    clean up card UI

--- src/Components/RetroItemCard/index.tsx
+++ src/Components/RetroItemCard/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Card } from "react-bootstrap";
+import { Button, ButtonToolbar, Card } from "react-bootstrap";
 import { RetroItemCardProps } from "./types";
 
 const RetroItemCard: React.FC<RetroItemCardProps> = ({ item, onPublish }) => {
@@ -8,14 +8,21 @@ const RetroItemCard: React.FC<RetroItemCardProps> = ({ item, onPublish }) => {
   };
 
   return (
-    <Card className="mb-2 p-1">
-      <Card.Title>{item?.text}</Card.Title>
-      <Card.Subtitle>{item?.authorName ?? ""}</Card.Subtitle>
+    <Card className="mb-2 p-2">
+      {item?.text}
+      <br />
       {!item?.isPublished && onPublish ? (
-        <Button variant="primary" onClick={handlePublish}>
-          Publish
-        </Button>
-      ) : null}
+        <ButtonToolbar>
+          <Button variant="primary" onClick={handlePublish} className="mr-2">
+            Publish
+          </Button>
+          <Button variant="danger" onClick={() => null}>
+            Delete
+          </Button>
+        </ButtonToolbar>
+      ) : (
+        <em>{item?.authorName ?? ""}</em>
+      )}
     </Card>
   );
 };
